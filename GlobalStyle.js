b70b512df99a9b7ef474dd1977a8590494bf3ac2
Ali <83968556+this-Rezaei@users.noreply.github.com>
2023-02-22 10:34:21 +0330

    add homescreen

---
+++ GlobalStyle.js
@@ -0,0 +1,7 @@
+import { Platform, StyleSheet,StatusBar } from "react-native";
+export default StyleSheet.create({
+  droidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,37 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { View, Text, SafeAreaView, Image } from "react-native";
+import GlobalStyle from "../GlobalStyle";
+// import { ChevronDownIcon } from "react-native-heroicons/outline";
 const HomeScreen = () => {
   const navigation = useNavigation();
 
-//   useLayoutEffect(() => {
-   
-//   }, [])
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      //   headerTitle: "TESTING",
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-600">HomeScreen</Text>
-    </View>
+    <SafeAreaView style={GlobalStyle.droidSafeArea}>
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-8 w-8 bg-gray-300 p-4 rounded-full"
+        />
+        <View>
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now !</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            {/* <ChevronDownIcon size={20} color="#00ccbb"/> */}
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
