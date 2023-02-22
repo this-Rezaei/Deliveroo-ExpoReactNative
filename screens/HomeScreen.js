601453f53f448b758983ee6ce3dedecc0c040a56
Rezaei <ali.rezaei0021@gmail.com>
2023-02-22 16:53:49 +0330

    add title

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,13 +2,14 @@ import { useNavigation } from "@react-navigation/native";
 import React, { useLayoutEffect } from "react";
 import { View, Text, SafeAreaView, Image } from "react-native";
 import GlobalStyle from "../GlobalStyle";
-// import { ChevronDownIcon } from "react-native-heroicons/outline";
+import IconEvil from "react-native-vector-icons/EvilIcons";
+import IconAntd from "react-native-vector-icons/AntDesign";
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      //   headerTitle: "TESTING",
+        // headerTitle: "TESTING",
       headerShown: false,
     });
   }, []);
@@ -16,20 +17,21 @@ const HomeScreen = () => {
   return (
     <SafeAreaView style={GlobalStyle.droidSafeArea}>
       {/* Header */}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
         <Image
           source={{
-            uri: "https://links.papareact.com/wru",
+            uri: "https://play-lh.googleusercontent.com/96JMV4psTHKt8Luv0GzpEWkoUoeGJvt8WrrV2eDqbcQe6jcfjfjmwwhe9UavwwdjGg",
           }}
           className="h-8 w-8 bg-gray-300 p-4 rounded-full"
         />
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver Now !</Text>
           <Text className="font-bold text-xl">
             Current Location
-            {/* <ChevronDownIcon size={20} color="#00ccbb"/> */}
+            <IconEvil name="chevron-down" size={20} color="#00ccbb" />
           </Text>
         </View>
+        <IconAntd name="user" size={30} color="#00ccbb" />
       </View>
     </SafeAreaView>
   );
