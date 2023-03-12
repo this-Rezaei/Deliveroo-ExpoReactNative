f79d13be58ffd987c2c49a8877e569f838897e09
Ali <83968556+this-Rezaei@users.noreply.github.com>
2023-03-12 13:39:42 +0330

    run studio sanity

--- components/CategoriesCard.js
+++ components/CategoriesCard.js
@@ -4,9 +4,9 @@ import React from "react";
 const CategoriesCard = ({ imgUrl, title }) => {
   const newLocal = "";
   return (
-    <TouchableOpacity className="mr-2 relative ">
-      <Image className="w-20 h-20 rounded" source={{ uri: imgUrl }} />
-      <Text className="absolute bottom-1 left-1 text-white font-bold">{title}</Text>
+    <TouchableOpacity className="mr-2  bg-white rounded-sm ">
+      <Image className="w-20 h-20 " source={{ uri: imgUrl }} />
+      <Text className="text-center mb-1 font-bold">{title}</Text>
     </TouchableOpacity>
   );
 };
--- sanity.js
+++ sanity.js
@@ -8,5 +8,8 @@ export const client = createClient({
   apiVersion: "2022-01-12", // use current date (YYYY-MM-DD) to target the latest API version
   // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
 });
- const builder =ImageUrlBuilder(client);
-//  export const urlFor
+const builder = ImageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
+
+export default client; 
